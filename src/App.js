e8c88f93ed2ad2db61d5f29ee5480025251f5f3d
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-25 23:18:37 +0530

    state connected

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const incident = 'Incident';
+  const count = useSelector((state) => state.count);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Days Since Last {incident}</h1>
+      <p>{count}</p>
+      <section>
+        <button>Increment</button>
+        <button>Reset</button>
+        <button>Decrement</button>
+      </section>
+    </main>
   );
 }
 
